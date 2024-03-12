8c83477992be9b3fb749d4d8c5718c5286871bf6
A-Goffard <aintzagoffard@gmail.com>
2024-03-12 12:54:55 +0100

    empezando navegador

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Nav />
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue";
+
+export default {
+  components: {
+    Nav,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #581446;
+}
+
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="banner" id="banner">
+        <button class="boton" id="">Inicio</button>
+        <button class="boton" id="">Sobre mi</button>
+        <button class="boton" id="">Currículum</button>
+        <button class="boton" id="">Skills</button>
+        <button class="boton" id="">Proyectos</button>
+        <button class="boton" id="">Contacto</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.banner {
+    width: 100%;
+    height: 10rem;
+    background-image: url('/src/assets/banner.png'); 
+    background-size: cover; 
+    background-position: center; 
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+}
+.boton {
+    padding: 0.7rem 1.2rem;
+
+    border-radius: 0.5rem;
+    background-color: rgb(184, 53, 184);
+    color: white;
+    font-size: large;
+}
+</style>
